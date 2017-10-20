8a70d8213bbc29486abd2b37d9b88dfaf40fcf4c
Javier Ventajas Hernandez <javier.ventajas@scout24.ch>
2017-10-20 14:46:10 +0200

    Fix formatting

--- classifier.py
+++ classifier.py
@@ -9,10 +9,8 @@ MIN_CONTOUR_AREA = 100
 
 class ImageClassifier:
 
-
     def __init__(self):
         self.knn = cv2.KNearest()
-
 
     def train(self, label_path, training_set_path):
 
@@ -22,32 +20,27 @@ class ImageClassifier:
 
         self.knn.train(training_set, labels)
 
-
     def recognize(self, image):
 
         final_string = ""
-
         preprocessed_image = self._preprocess_image(image)
-
         valid_contours = self._find_valid_contours(preprocessed_image.copy())
 
         for contour in valid_contours:
-
             cv2.rectangle(image,
                           (contour.x, contour.y),
                           (contour.x + contour.width, contour.y + contour.height),
-                          (0, 255, 0),2)
+                          (0, 255, 0), 2)
 
             crop = preprocessed_image[contour.y: contour.y + contour.height, contour.x: contour.x + contour.width]
             resized_crop = cv2.resize(crop, (RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT))
             flattened_crop = resized_crop.reshape((1, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))
             float_array = np.float32(flattened_crop)
-            value, npaResults, neigh_resp, dists = self.knn.find_nearest(float_array, k=1)
-            current_char = str(chr(int(npaResults[0][0])))
+            value, npa_results, neigh_resp, dists = self.knn.find_nearest(float_array, k=1)
+            current_char = str(chr(int(npa_results[0][0])))
             final_string = final_string + current_char
 
         return final_string, image
-
 
     def _find_valid_contours(self, image):
 
@@ -59,7 +52,6 @@ class ImageClassifier:
 
         return valid_contours
 
-
     def _preprocess_image(self, image):
 
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
@@ -69,7 +61,8 @@ class ImageClassifier:
         grayscale_image = cv2.cvtColor(eroded_image, cv2.COLOR_BGR2GRAY)
         blurred_image = cv2.GaussianBlur(grayscale_image, (5, 5), 0)
 
-        thresholded_image = cv2.adaptiveThreshold(blurred_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+        thresholded_image = cv2.adaptiveThreshold(blurred_image, 255,
+                                                  cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
         return thresholded_image
 
